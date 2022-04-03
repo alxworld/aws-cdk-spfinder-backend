7a44dbf1a61d8abeac2c2bbcfd265a3aba088b09
alxworld <jsalex@gmail.com>
2022-04-03 20:03:38 +0530

    api-gateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,9 +2,12 @@ import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 
 export class SpaceStack extends Stack {
     
+    private api = new RestApi(this, 'SpaceApi');
+
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
         
@@ -13,6 +16,13 @@ export class SpaceStack extends Stack {
             code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
             handler: 'hello.main'
         }) 
+
+        // Hello API Lambda integration ( linking API Gateway with Lambda )
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLamdaResource = this.api.root.addResource('hello');
+        helloLamdaResource.addMethod('GET', helloLambdaIntegration);
+
+    
     }
     
 }
